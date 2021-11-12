51caa3050537768862e5ad0946eca3598714f5a1
Enrico Florentino Gomes <enricogomes@live.com>
2021-11-12 20:07:00 +0000

    Done

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -9,12 +9,45 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+        
+    var responses:[Answer]
+    
+    init?(coder: NSCoder, responses:[Answer]){
+        self.responses=responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton=true
+        calculatePersonalityResult()
         // Do any additional setup after loading the view.
     }
     
+    func calculatePersonalityResult(){
+        let frequencyOfAnswers = responses.reduce(into: [:]){
+            (counts, answer) in
+            counts[answer.type, default:0]+=1
+        }
+        
+        let frequentAnswersSorted=frequencyOfAnswers.sorted(by: {
+            (pair1,pair2) in
+            return pair1.value>pair2.value
+        })
+        
+        let mostCommonAnswer=frequentAnswersSorted.sorted{
+            $0.1 > $1.1
+        }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
 
     /*
     // MARK: - Navigation
